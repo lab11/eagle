ee4be6d79c67f3753ad8d57bffc16198d596fda0
Brad Campbell <bradjc5@gmail.com>
2013-12-29 23:47:05 -0500

    now saves local changes to .info files

--- scripts/eagle_attributes.py
+++ scripts/eagle_attributes.py
@@ -9,6 +9,8 @@ import sys
 # Get all of the tricky packages out of the way
 try:
 	import sh
+	from sh import mktemp
+	from sh import rm
 except:
 	print("You need to install the sh module.")
 	print("https://github.com/amoffat/sh")
@@ -21,10 +23,14 @@ except:
 	print("export PATH=$PATH:~/eagle-6.5.0/bin")
 	sys.exit(1)
 
-from glob import glob
+import glob
 import os
-import sys
-import sh
+
+def read_in_info (filename, d):
+	with open(filename) as f:
+		for line in f:
+			opts = line.split(':', 1)
+			d[opts[0].strip().lower()] = opts[1].strip()
 
 # Display the help if any arguments are provided.
 if len(sys.argv) > 1:
@@ -32,10 +38,10 @@ if len(sys.argv) > 1:
 	sys.exit(0)
 
 # Determine the location of this script and the .ulp file
-here = os.path.dirname(os.path.realpath(__file__))
-ulp = os.path.join(here, '..', 'ulp', 'attributes.ulp')
-scr = os.path.join(here, '..', 'scr', 'ulp-sch.scr')
-tmpscr = os.path.join('/', 'tmp', 'ulp.scr')
+here   = os.path.dirname(os.path.realpath(__file__))
+ulp    = os.path.join(here, '..', 'ulp', 'attributes.ulp')
+scr    = os.path.join(here, '..', 'scr', 'ulp-sch.scr')
+tmpscr = str(mktemp(os.path.join('/', 'tmp', 'ulp.scrXXXX'))).strip()
 
 contents = ''
 with open(scr, 'r') as f:
@@ -47,6 +53,25 @@ with open(tmpscr, 'w') as f:
 	f.write(contents)
 
 # Figure out the name of the schematic to run this on.
-for sch in glob('*.sch'):
-	# Attributes to .info file
+for sch in glob.glob('*.sch'):
+	attrs = {}
+
+	sch_name = sch[:-4]
+
+	# Read in the old .info file to save the original values
+	if os.path.exists(sch_name + '.info'):
+		read_in_info(sch_name + '.info', attrs)
+
+	# Generate a new .info file in case anything changed
 	eagle('-S', tmpscr, sch)
+
+	# Read the new .info file in to update any changed values
+	read_in_info(sch_name + '.info', attrs)
+
+	# Write out the key values
+	with open(sch_name + '.info', 'w') as f:
+		for k,v in attrs.items():
+			f.write('{}: {}\n'.format(k, v))
+
+
+rm(tmpscr)
