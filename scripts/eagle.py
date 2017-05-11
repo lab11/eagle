9ddbc4f7e20ae1e6aa28c9b02fd6620bf0cd1469
Pat Pannuto <pat.pannuto@gmail.com>
2017-05-11 17:05:31 -0400

    try to automatically add eagle to PATH

--- scripts/eagle.py
+++ scripts/eagle.py
@@ -4,8 +4,40 @@ HELP = """
 Run all of the scripts relevant to eagle in the correct order.
 """
 
+import glob
 import os
+import platform
 import sys
+
+# Get all of the tricky packages out of the way
+try:
+	import sh
+except:
+	print("You need to install the sh module.")
+	print("https://github.com/amoffat/sh")
+	print("Try `pip install sh` or `pip3 install sh`")
+	sys.exit(1)
+
+try:
+	from sh import eagle
+except ImportError:
+	# Try to find Eagle if we can
+	print("You need to have eagle in your PATH, trying to find it...")
+	if platform.system() == 'Darwin':
+		versions = glob.glob('/Applications/EAGLE-*')
+		if len(versions):
+			versions.sort()
+			latest = versions[-1]
+			print('Using {}'.format(latest))
+			path = os.path.join(latest, 'EAGLE.app', 'Contents', 'MacOS')
+			os.environ['PATH'] += ':' + path
+
+	try:
+		from sh import eagle
+	except ImportError:
+		print("Could not find eagle automatically, need to add to PATH manually")
+		print("Something like `export PATH=$PATH:~/eagle-6.5.0/bin`")
+		sys.exit(1)
 
 #           filename               arguments
 scripts = [('eagle_centroid.py',   False),
